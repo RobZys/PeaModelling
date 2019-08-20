642f9409fb713436521776167b061ffb4d6e38b0
frank0434 <jian.liu@plantandfood.co.nz>
2019-08-21 11:49:06 +1200

    metadata

---
+++ metadata_management.R
@@ -0,0 +1,35 @@
+library(RSQLite)
+library(data.table)
+library(dplyr, warn.conflicts = FALSE)
+
+url <- "https://iplant.plantandfood.co.nz/project/P442060-13/_layouts/15/WopiFrame.aspx?sourcedoc=/project/P442060-13/Research/18_19_SAE_pea_protein_managment.xlsx&action=default"
+Source_HB = "RAW_HB"
+Source_Linc = "RAW_Lincoln"
+
+# Use the function scrape_list to extract the table from iPlant as a dataframe.
+df_lincoln <- scrape_xl(url = url, sheet = Source_Linc, skip = 2)
+df_HB <- scrape_xl(url = url, sheet = Source_HB, skip = 2)
+var_names <- scrape_xl(url, sheet = "VariableNames", skip = 0)
+var_names
+var_LN <- colnames(df_lincoln)
+var_HB <- colnames(df_HB)
+
+nrow(var_names)
+var_combined <- data.table(var_names)[, Experiment := NULL]
+var_LN <- data.table(LN = var_LN)[, Site := "Lincoln"]
+var_HB <- data.table(HB = var_HB)[, Site := "HB"]
+
+var_LN <- merge(var_combined, var_LN, by.x = "Name", by.y = "LN", all = T , no.dups = T)
+var_combined <- merge(var_LN, var_HB, by.x = "Name", by.y = "HB", all = T , no.dups = T )
+colnames(var_combined)
+setcolorder(var_combined, colnames(var_combined)[c(1,6,7,2:5)])
+
+
+# create a sqlite ---------------------------------------------------------
+
+con <- dbConnect(RSQLite::SQLite(),'./Pea_metadata.sqlite3')
+setkey(var_combined, Name)
+anyDuplicated(var_combined)
+dbWriteTable(con,'metadata',var_combined, overwrite = TRUE)
+dbReadTable(con, 'metadata')
+d
